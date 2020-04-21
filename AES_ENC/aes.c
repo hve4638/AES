7a309d3f3027d31766e476fdbf27f567765c776e
hve4638 <47706364+hve4638@users.noreply.github.com>
2020-04-21 10:56:54 +0900

    암호화구현

--- AES_ENC/aes.c
+++ AES_ENC/aes.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include "aes.h"
 #define EncCalculateT(i) \
@@ -299,20 +300,37 @@ void AES_DEC_Optimization(u8 CT[16], u32 W[], u8 PT[16], int keysize)
 }
 #pragma endregion
 
+void XOR16Bytes(u8 S[16], u8 RK[16])
+{
+	S[0] ^= RK[0];
+	S[1] ^= RK[1];
+	S[2] ^= RK[2];
+	S[3] ^= RK[3];
+	S[4] ^= RK[4];
+	S[5] ^= RK[5];
+	S[6] ^= RK[6];
+	S[7] ^= RK[7];
+	S[8] ^= RK[8];
+	S[9] ^= RK[9];
+	S[10] ^= RK[10];
+	S[11] ^= RK[11];
+	S[12] ^= RK[12];
+	S[13] ^= RK[13];
+	S[14] ^= RK[14];
+	S[15] ^= RK[15];
+}
+
 void ECB_Encryption(char* inputfile, char* outputfile, u32 W[])
 {
-	FILE *rfp, *wfp;
-	u8 *inputbuf, *outputbuf, r;
+	FILE* rfp, * wfp;
+	u8* inputbuf, * outputbuf, r;
 	u32 DataLen;
+	u8 IV[16] = { 0x00 };
+	int i;
 
 	fopen_s(&rfp, inputfile, "rb");
 	if (rfp == NULL)
-		perror("fopen_s fail!\n");
-
-	fopen_s(&wfp, outputfile, "rb");
-	if (wfp == NULL)
-		perror("fopen_s fail!\n");
-	
+		perror("fopen_s 실패! (input)\n");
 	fseek(rfp, 0, SEEK_END);
 	DataLen = ftell(rfp);
 	fseek(rfp, 0, SEEK_SET);
@@ -322,62 +340,108 @@ void ECB_Encryption(char* inputfile, char* outputfile, u32 W[])
 	inputbuf = calloc(DataLen + r, sizeof(u8));
 	outputbuf = calloc(DataLen + r, sizeof(u8));
 	fread(inputbuf, 1, DataLen, rfp);
+	fclose(rfp);
+
 	memset(inputbuf + DataLen, r, r);
 
+	for (i = 0; i < (DataLen + r) / 16; i++)
+	{
+		AES_ENC_Optimization(inputbuf + 16 * i, W, outputbuf + 16 * i, 128);
+	}
 
+	fopen_s(&wfp, outputfile, "wb");
+	if (wfp == NULL)
+		perror("fopen_s 실패! (output)\n");
+	fwrite(outputbuf, 1, DataLen + r, wfp);
+
+	fclose(wfp);
+	
+}
+
+void CBC_Encryption(char* inputfile, char* outputfile, u32 W[])
+{
+	FILE* rfp, * wfp;
+	u8* inputbuf, * outputbuf, r;
+	u32 DataLen;
+	u8 IV[16] = { 0x00 };
+	int i;
+
+	fopen_s(&rfp, inputfile, "rb");
+	if (rfp == NULL)
+		perror("fopen_s 실패! (input)\n");
+	fseek(rfp, 0, SEEK_END);
+	DataLen = ftell(rfp);
+	fseek(rfp, 0, SEEK_SET); 
+
+	r = 16 - DataLen % 16; //PKCS #7 Padding
+
+	inputbuf = calloc(DataLen + r, sizeof(u8));
+	outputbuf = calloc(DataLen + r, sizeof(u8));
+	fread(inputbuf, 1, DataLen, rfp);
 	fclose(rfp);
+
+	memset(inputbuf + DataLen, r, r);
+
+	XOR16Bytes(inputbuf, IV);
+	AES_ENC_Optimization(inputbuf, W, outputbuf, 128);
+
+	for (i = 1; i < (DataLen + r) / 16; i++)
+	{
+		XOR16Bytes(inputbuf + 16 * i, outputbuf + 16 * (i-1));
+		AES_ENC_Optimization(inputbuf + 16 * i, W, outputbuf + 16 * i, 128);
+	}
+
+	fopen_s(&wfp, outputfile, "wb");
+	if (wfp == NULL)
+		perror("fopen_s 실패! (output)\n");
+	fwrite(outputbuf, 1, DataLen + r, wfp);
+
 	fclose(wfp);
 }
 
-
 int main(int argc, char* argv[])
 {
-	u8 PT[16] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
 	u8 ENCKEY[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-	u8 RK[240];
-	u32 W[60];
-
-	u8 CT[16], DT[16], DT2[16];
 	int keysize = 128;
+	u32 W[60];
+	printf("AES Encryption progress...\n");
 
-	if (strcmp(argv[1], 'ecb') == 0)
+	if (strcmp(argv[1], "ecb") == 0)
 	{
-		AES_KeySchedule_Optimization(MK, W, keysize);
+		AES_KeySchedule_Optimization(ENCKEY, W, keysize);
 		ECB_Encryption(argv[2], argv[3], W);
 	}
-	else if (strcmp(argv[1], 'cbc') == 0)
+	else if (strcmp(argv[1], "ecb") == 0)
 	{
-		AES_DEC_Optimization(CT, W, DT2, keysize);
-		ECB_Encryption(argv[2], argv[3], W);
-
+		AES_KeySchedule_Optimization(ENCKEY, W, keysize);
+		CBC_Encryption(argv[2], argv[3], W);
 	}
-
-	AES_ENC_Optimization(PT, W, CT, keysize);
-	/*
-	AES_DEC(CT, RK, DT, keysize);
-	AES_DEC_Optimization(CT, W, DT2, keysize);
-
-	printf("PT : "); print16(PT, 16); printf("\n");
-	printf("CT : "); print16(CT, 16); printf("\n");
-	printf("DEC: "); print16(DT, 16); printf("\n");
-	printf("DEC: "); print16(DT2, 16); printf("\n");
-
-	
-	st = clock();
-	for (i = 0; i < 10000; i++)
-		AES_DEC(CT, RK, CT, keysize);
-	fn = clock();
-	printf("DES    : %f\n", (double)(fn - st) / CLOCKS_PER_SEC);
-
-	st = clock();
-	for (i = 0; i < 10000; i++)
-		AES_DEC_Optimization(CT, W, CT, keysize);
-	fn = clock();
-	printf("DES_Op : %f", (double)(fn - st) / CLOCKS_PER_SEC);
-
-
-	*/
 	return 0;
 }
 
 
+
+/*
+AES_DEC(CT, RK, DT, keysize);
+AES_DEC_Optimization(CT, W, DT2, keysize);
+
+printf("PT : "); print16(PT, 16); printf("\n");
+printf("CT : "); print16(CT, 16); printf("\n");
+printf("DEC: "); print16(DT, 16); printf("\n");
+printf("DEC: "); print16(DT2, 16); printf("\n");
+
+
+st = clock();
+for (i = 0; i < 10000; i++)
+	AES_DEC(CT, RK, CT, keysize);
+fn = clock();
+printf("DES    : %f\n", (double)(fn - st) / CLOCKS_PER_SEC);
+
+st = clock();
+for (i = 0; i < 10000; i++)
+	AES_DEC_Optimization(CT, W, CT, keysize);
+fn = clock();
+printf("DES_Op : %f", (double)(fn - st) / CLOCKS_PER_SEC);
+
+
+*/
