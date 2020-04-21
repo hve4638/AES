5a218517b4a64efd9ff114f384b950178975bfb9
hve4638 <47706364+hve4638@users.noreply.github.com>
2020-04-21 09:32:14 +0900

    12

--- AES_ENC/aes.c
+++ AES_ENC/aes.c
@@ -19,27 +19,43 @@
 	s2 = (Te2[(t2 >> 24)] & 0xff000000) ^ (Te3[(t3 >> 16) & 0xff] & 0x00ff0000) ^ (Te0[(t0 >> 8) & 0xff] & 0x0000ff00) ^ (Te1[t1 & 0xff] & 0x000000ff) ^ W[i+2];	\
 	s3 = (Te2[(t3 >> 24)] & 0xff000000) ^ (Te3[(t0 >> 16) & 0xff] & 0x00ff0000) ^ (Te0[(t1 >> 8) & 0xff] & 0x0000ff00) ^ (Te1[t2 & 0xff] & 0x000000ff) ^ W[i+3];
 
+#define DecInvSbox(i) \
+	t0 = (Sbox_inv[u8byte(s0, 3)] << 24) ^ (Sbox_inv[u8byte(s0, 2)] << 16) ^ (Sbox_inv[u8byte(s0, 1)] << 8) ^ (Sbox_inv[u8byte(s0, 0)]); \
+	t1 = (Sbox_inv[u8byte(s1, 3)] << 24) ^ (Sbox_inv[u8byte(s1, 2)] << 16) ^ (Sbox_inv[u8byte(s1, 1)] << 8) ^ (Sbox_inv[u8byte(s1, 0)]); \
+	t2 = (Sbox_inv[u8byte(s2, 3)] << 24) ^ (Sbox_inv[u8byte(s2, 2)] << 16) ^ (Sbox_inv[u8byte(s2, 1)] << 8) ^ (Sbox_inv[u8byte(s2, 0)]); \
+	t3 = (Sbox_inv[u8byte(s3, 3)] << 24) ^ (Sbox_inv[u8byte(s3, 2)] << 16) ^ (Sbox_inv[u8byte(s3, 1)] << 8) ^ (Sbox_inv[u8byte(s3, 0)]);
+#define DecInvShiftRow(i) \
+	t0 = (u8byte(s0, 3) << 24) ^ (u8byte(s3, 2) << 16) ^ (u8byte(s2, 1) << 8) ^ (u8byte(s1, 0)); \
+	t1 = (u8byte(s1, 3) << 24) ^ (u8byte(s0, 2) << 16) ^ (u8byte(s3, 1) << 8) ^ (u8byte(s2, 0)); \
+	t2 = (u8byte(s2, 3) << 24) ^ (u8byte(s1, 2) << 16) ^ (u8byte(s0, 1) << 8) ^ (u8byte(s3, 0)); \
+	t3 = (u8byte(s3, 3) << 24) ^ (u8byte(s2, 2) << 16) ^ (u8byte(s1, 1) << 8) ^ (u8byte(s0, 0));
 #define DecCalculateT(i) \
-	t0 = Td0[s0 >> 24] ^ Td1[(s1 >> 16) & 0xff] ^ Td2[(s2 >> 8) & 0xff] ^ Td3[s3 & 0xff] ^ W[i];		\
-	t1 = Td0[s3 >> 24] ^ Td1[(s0 >> 16) & 0xff] ^ Td2[(s1 >> 8) & 0xff] ^ Td3[s2 & 0xff] ^ W[i + 1];	\
-	t2 = Td0[s2 >> 24] ^ Td1[(s3 >> 16) & 0xff] ^ Td2[(s0 >> 8) & 0xff] ^ Td3[s1 & 0xff] ^ W[i + 2];	\
-	t3 = Td0[s1 >> 24] ^ Td1[(s2 >> 16) & 0xff] ^ Td2[(s3 >> 8) & 0xff] ^ Td3[s0 & 0xff] ^ W[i + 3];
+	t0 = (Sbox_inv[u8byte(s0, 3)] << 24) ^ (Sbox_inv[u8byte(s3, 2)] << 16) ^ (Sbox_inv[u8byte(s2, 1)] << 8) ^ Sbox_inv[u8byte(s1, 0)] ^ W[i];		\
+	t1 = (Sbox_inv[u8byte(s1, 3)] << 24) ^ (Sbox_inv[u8byte(s0, 2)] << 16) ^ (Sbox_inv[u8byte(s3, 1)] << 8) ^ Sbox_inv[u8byte(s2, 0)] ^ W[i+1];		\
+	t2 = (Sbox_inv[u8byte(s2, 3)] << 24) ^ (Sbox_inv[u8byte(s1, 2)] << 16) ^ (Sbox_inv[u8byte(s0, 1)] << 8) ^ Sbox_inv[u8byte(s3, 0)] ^ W[i+2];		\
+	t3 = (Sbox_inv[u8byte(s3, 3)] << 24) ^ (Sbox_inv[u8byte(s2, 2)] << 16) ^ (Sbox_inv[u8byte(s1, 1)] << 8) ^ Sbox_inv[u8byte(s0, 0)] ^ W[i+3];
 #define DecCalculateS(i) \
-	s0 = Td0[t0 >> 24] ^ Td1[(t1 >> 16) & 0xff] ^ Td2[(t2 >> 8) & 0xff] ^ Td3[t3 & 0xff] ^ W[i];		\
-	s1 = Td0[t3 >> 24] ^ Td1[(t0 >> 16) & 0xff] ^ Td2[(t1 >> 8) & 0xff] ^ Td3[t2 & 0xff] ^ W[i + 1];	\
-	s2 = Td0[t2 >> 24] ^ Td1[(t3 >> 16) & 0xff] ^ Td2[(t0 >> 8) & 0xff] ^ Td3[t1 & 0xff] ^ W[i + 2];	\
-	s3 = Td0[t1 >> 24] ^ Td1[(t2 >> 16) & 0xff] ^ Td2[(t3 >> 8) & 0xff] ^ Td3[t0 & 0xff] ^ W[i + 3];
+	s0 = (Sbox_inv[u8byte(t0, 3)] << 24) ^ (Sbox_inv[u8byte(t3, 2)] << 16) ^ (Sbox_inv[u8byte(t2, 1)] << 8) ^ Sbox_inv[u8byte(t1, 0)] ^ W[i];		\
+	s1 = (Sbox_inv[u8byte(t1, 3)] << 24) ^ (Sbox_inv[u8byte(t0, 2)] << 16) ^ (Sbox_inv[u8byte(t3, 1)] << 8) ^ Sbox_inv[u8byte(t2, 0)] ^ W[i+1];		\
+	s2 = (Sbox_inv[u8byte(t2, 3)] << 24) ^ (Sbox_inv[u8byte(t1, 2)] << 16) ^ (Sbox_inv[u8byte(t0, 1)] << 8) ^ Sbox_inv[u8byte(t3, 0)] ^ W[i+2];		\
+	s3 = (Sbox_inv[u8byte(t3, 3)] << 24) ^ (Sbox_inv[u8byte(t2, 2)] << 16) ^ (Sbox_inv[u8byte(t1, 1)] << 8) ^ Sbox_inv[u8byte(t0, 0)] ^ W[i+3];
+#define DecMixColumnS() \
+	s0 = Td0[t0 >> 24] ^ Td1[(t0 >> 16) & 0xff] ^ Td2[(t0 >> 8) & 0xff] ^ Td3[t0 & 0xff]; \
+	s1 = Td0[t1 >> 24] ^ Td1[(t1 >> 16) & 0xff] ^ Td2[(t1 >> 8) & 0xff] ^ Td3[t1 & 0xff]; \
+	s2 = Td0[t2 >> 24] ^ Td1[(t2 >> 16) & 0xff] ^ Td2[(t2 >> 8) & 0xff] ^ Td3[t2 & 0xff]; \
+	s3 = Td0[t3 >> 24] ^ Td1[(t3 >> 16) & 0xff] ^ Td2[(t3 >> 8) & 0xff] ^ Td3[t3 & 0xff];
+
+
 #pragma region
 void RoundKeyGeneration128(u8[], u8[]);
 void AES_KeySchedule(u8[], u8[], int);
-
 void AES_ENC(u8[16], u8[], u8[], int);
 void AES_ENC_Optimization(u8[16], u32[], u8[16], int);
 void RoundKeyGeneration128_Optimization(u8[], u32[]);
 void AES_KeySchedule_Optimization(u8[], u32[], int);
 
 void AES_DEC(u8[16], u8[], u8[], int);
-void AES_DNC_Optimization(u8[16], u32[], u8[16], int);
+void AES_DEC_Optimization(u8[16], u32[], u8[16], int);
 
 void print16(u8*, int);
 void print32(u32*, int);
@@ -224,64 +240,144 @@ void AES_ENC_Optimization(u8 PT[16], u32 W[], u8 CT[16], int keysize)
 }
 void AES_DEC_Optimization(u8 CT[16], u32 W[], u8 PT[16], int keysize)
 {
-	//죄송합니다 최적화 구현을 못했습니다
+	int i;
 	int Nr = keysize / 32 + 6;
-	u8 temp[16];
 	u32 s0, s1, s2, s3, t0, t1, t2, t3;
-	int i;
-	copyarray(temp, CT);
+	u8 temp[16];
+	s0 = u4byte_in(CT) ^ W[Nr * 4];
+	s1 = u4byte_in(CT + 4) ^ W[Nr * 4 + 1];
+	s2 = u4byte_in(CT + 8) ^ W[Nr * 4 + 2];
+	s3 = u4byte_in(CT + 12) ^ W[Nr * 4 + 3];
 
-	i = Nr * 4;
-	AddRoundKey32(temp, W + i); i -= 4;
-	ShiftRows_inv(temp);
-	SubBytes_inv(temp);
-	AddRoundKey(temp, W + i); i -= 4;
-
-	s0 = u4byte_in(temp) ^ W[i];
-	s1 = u4byte_in(temp + 4) ^ W[i + 1];
-	s2 = u4byte_in(temp + 8) ^ W[i + 2];
-	s3 = u4byte_in(temp + 12) ^ W[i + 3];
-
+	i = Nr * 4 - 4;
+	//1 round
 	DecCalculateT(i); i -= 4;
-	DecCalculateS(i); i -= 4;
+	DecMixColumnS();
+
+	//2 round
 	DecCalculateT(i); i -= 4;
-	DecCalculateS(i); i -= 4;
+	DecMixColumnS();
+
+	//3 round
 	DecCalculateT(i); i -= 4;
-	DecCalculateS(i); i -= 4;
+	DecMixColumnS();
+
+	//4 round
 	DecCalculateT(i); i -= 4;
-	DecCalculateS(i); i -= 4;
-	
-	u4byte_out(temp, s0);
-	u4byte_out(temp + 4, s1);
-	u4byte_out(temp + 8, s2);
-	u4byte_out(temp + 12, s3);
+	DecMixColumnS();
 
-	ShiftRows_inv(temp);
-	SubBytes_inv(temp);
-	AddRoundKey(temp, W + i);
+	//5 round
+	DecCalculateT(i); i -= 4;
+	DecMixColumnS();
 
-	copyarray(PT, temp);
-}
-#pragma endregion
+	//6 round
+	DecCalculateT(i); i -= 4;
+	DecMixColumnS();
 
-int main()
-{
-	u8 PT[16] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
-	u8 PT2[16] = { 0xa6, 0xa6, 0x64, 0xa4, 0x84, 0x4a, 0x4e, 0x7b, 0xa1, 0x87, 0x3b, 0x87, 0xc6, 0xd1, 0x82, 0x1e };
-	u8 ENCKEY[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
-	u8 RK[240], CT[16], DT[16];
-	u32 W[60] = { 0x00 };
-	int i;
-	int keysize = 128;
+	//7 round
+	DecCalculateT(i); i -= 4;
+	DecMixColumnS();
 
-	AES_KeySchedule(ENCKEY, RK, keysize);
-	AES_KeySchedule_Optimization(ENCKEY, W, keysize);
-	AES_ENC_Optimization(PT, W, CT, keysize);
-	AES_DEC(CT, RK, DT, keysize);
-	//AES_DEC_Optimization(CT, W, DT, keysize);
-	printf("ORG: "); print16(PT, 16); printf("\n");
-	printf("ENC: "); print16(CT, 16); printf("\n");
-	printf("DEC: "); print16(DT, 16); printf("\n");
+	//8 round
+	DecCalculateT(i); i -= 4;
+	DecMixColumnS();
+
+	//9 round
+	DecCalculateT(i); i -= 4;
+	DecMixColumnS();
+
+	//10 round
+	DecCalculateT(i); i -= 4;
+
+
+	u4byte_out(PT, t0);
+	u4byte_out(PT + 4, t1);
+	u4byte_out(PT + 8, t2);
+	u4byte_out(PT + 12, t3);
 
 	return 0;
 }
+#pragma endregion
+
+void ECB_Encryption(char* inputfile, char* outputfile, u32 W[])
+{
+	FILE *rfp, *wfp;
+	u8 *inputbuf, *outputbuf, r;
+	u32 DataLen;
+
+	fopen_s(&rfp, inputfile, "rb");
+	if (rfp == NULL)
+		perror("fopen_s fail!\n");
+
+	fopen_s(&wfp, outputfile, "rb");
+	if (wfp == NULL)
+		perror("fopen_s fail!\n");
+	
+	fseek(rfp, 0, SEEK_END);
+	DataLen = ftell(rfp);
+	fseek(rfp, 0, SEEK_SET);
+
+	r = 16 - DataLen % 16; //PKCS #7 Padding
+
+	inputbuf = calloc(DataLen + r, sizeof(u8));
+	outputbuf = calloc(DataLen + r, sizeof(u8));
+	fread(inputbuf, 1, DataLen, rfp);
+	memset(inputbuf + DataLen, r, r);
+
+
+	fclose(rfp);
+	fclose(wfp);
+}
+
+
+int main(int argc, char* argv[])
+{
+	u8 PT[16] = { 0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a };
+	u8 ENCKEY[16] = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+	u8 RK[240];
+	u32 W[60];
+
+	u8 CT[16], DT[16], DT2[16];
+	int keysize = 128;
+
+	if (strcmp(argv[1], 'ecb') == 0)
+	{
+		AES_KeySchedule_Optimization(MK, W, keysize);
+		ECB_Encryption(argv[2], argv[3], W);
+	}
+	else if (strcmp(argv[1], 'cbc') == 0)
+	{
+		AES_DEC_Optimization(CT, W, DT2, keysize);
+		ECB_Encryption(argv[2], argv[3], W);
+
+	}
+
+	AES_ENC_Optimization(PT, W, CT, keysize);
+	/*
+	AES_DEC(CT, RK, DT, keysize);
+	AES_DEC_Optimization(CT, W, DT2, keysize);
+
+	printf("PT : "); print16(PT, 16); printf("\n");
+	printf("CT : "); print16(CT, 16); printf("\n");
+	printf("DEC: "); print16(DT, 16); printf("\n");
+	printf("DEC: "); print16(DT2, 16); printf("\n");
+
+	
+	st = clock();
+	for (i = 0; i < 10000; i++)
+		AES_DEC(CT, RK, CT, keysize);
+	fn = clock();
+	printf("DES    : %f\n", (double)(fn - st) / CLOCKS_PER_SEC);
+
+	st = clock();
+	for (i = 0; i < 10000; i++)
+		AES_DEC_Optimization(CT, W, CT, keysize);
+	fn = clock();
+	printf("DES_Op : %f", (double)(fn - st) / CLOCKS_PER_SEC);
+
+
+	*/
+	return 0;
+}
+
+
